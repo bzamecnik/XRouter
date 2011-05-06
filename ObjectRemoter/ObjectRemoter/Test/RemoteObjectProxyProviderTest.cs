2ccb892ef3f1b433f91a6b2aac73aa3895dbeb6e
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2011-05-06 18:58:03 +0200

    Add some new RemoteObjectProxyProvider unit tests.
    Mostly bad-day tests.h

--- ObjectRemoter/ObjectRemoter/Test/RemoteObjectProxyProviderTest.cs
+++ ObjectRemoter/ObjectRemoter/Test/RemoteObjectProxyProviderTest.cs
@@ -9,11 +9,46 @@ namespace ObjectRemoter.Test
     class RemoteObjectProxyProviderTest
     {
         #region Happy-day test methods
+
+        [Fact, Trait("Category", "Network")]
+        public void GetProxy()
+        {
+            SampleInvocable sampleInvocable = new SampleInvocable();
+            RemoteObjectAddress address = ObjectServer.PublishObject(sampleInvocable);
+            IInvocable proxy = RemoteObjectProxyProvider.GetProxy<IInvocable>(
+                address, typeof(IInvocable));
+            Assert.NotNull(proxy);
+        }
+
+        // required interface type is descendant of type parameter
+        [Fact, Trait("Category", "Network")]
+        public void GetProxyWithDescendantRequiredInterface()
+        {
+            SampleInvocable sampleInvocable = new SampleInvocable();
+            RemoteObjectAddress address = ObjectServer.PublishObject(sampleInvocable);
+            IRemotelyReferable proxy = RemoteObjectProxyProvider.GetProxy<IRemotelyReferable>(
+                    address, typeof(IInvocable));
+            Assert.NotNull(proxy);
+        }
+
+        [Fact, Trait("Category", "Network")]
+        public void GetProxyWithNullInterfaceType()
+        {
+            SampleInvocable sampleInvocable = new SampleInvocable();
+            RemoteObjectAddress address = ObjectServer.PublishObject(sampleInvocable);
+            IInvocable proxy = RemoteObjectProxyProvider.GetProxy<IInvocable>(address, null);
+            Assert.NotNull(proxy);
+        }
+
+        // TODO:
+        // - try a class with custom interface
+        // - local address
+
         #endregion
 
         #region Bad-day test methods
 
-        [Fact]
+        [Fact, Trait("Category", "Network")]
         public void GetProxyWithNullAddress()
         {
             Assert.Throws<ArgumentNullException>(() =>
@@ -21,11 +56,71 @@ namespace ObjectRemoter.Test
             );
         }
 
+        // requiredInterface is not an interface
+        [Fact, Trait("Category", "Network")]
+        public void GetProxyWithNonInterfaceType()
+        {
+            SampleInvocable sampleInvocable = new SampleInvocable();
+            RemoteObjectAddress address = ObjectServer.PublishObject(sampleInvocable);
+            Assert.Throws<InvalidOperationException>(() =>
+                RemoteObjectProxyProvider.GetProxy<IInvocable>(
+                    address, typeof(SampleInvocable))
+            );
+        }
+
+        // type parameter type is not an interface
+        [Fact, Trait("Category", "Network")]
+        public void GetProxyWithNonInterfaceTypeParameter()
+        {
+            SampleInvocable sampleInvocable = new SampleInvocable();
+            RemoteObjectAddress address = ObjectServer.PublishObject(sampleInvocable);
+            Assert.Throws<InvalidOperationException>(() =>
+                RemoteObjectProxyProvider.GetProxy<SampleInvocable>(
+                    address, typeof(IInvocable))
+            );
+        }
+
+        // required interface type is not assignable from proxy type
+        // (is completely different)
+        [Fact, Trait("Category", "Network")]
+        public void GetProxyWithIncompatibleInterface()
+        {
+            SampleInvocable sampleInvocable = new SampleInvocable();
+            RemoteObjectAddress address = ObjectServer.PublishObject(sampleInvocable);
+            // pass a different interface parameter then is the interface of the proxy
+            // (eg. IRemotelyCloneable instead of IInvocable)
+            Assert.Throws<ArgumentException>(() =>
+                RemoteObjectProxyProvider.GetProxy<IInvocable>(
+                    address, typeof(IRemotelyCloneable))
+            );
+        }
+
+        // required interface type is not assignable from proxy type
+        // (is antecendant of type parameter)
+        [Fact, Trait("Category", "Network")]
+        public void GetProxyWithAntecendantRequiredInterface()
+        {
+            SampleInvocable sampleInvocable = new SampleInvocable();
+            RemoteObjectAddress address = ObjectServer.PublishObject(sampleInvocable);
+            Assert.Throws<ArgumentException>(() =>
+                RemoteObjectProxyProvider.GetProxy<IInvocable>(
+                address, typeof(IRemotelyReferable)));
+        }
+
         // TODO:
-        // - requiredInterface is not an interface -> InvalidOperationException
-        // - type parameter type is not an interface -> InvalidOperationException
-        // - requiredInterface is not assignable from proxyObject type -> ArgumentException
         // - ProxyInterceptor - System.Net.Sockets.SocketException
+
+        #endregion
+
+        #region Helper classes and interfaces
+
+        private class SampleInvocable : IInvocable
+        {
+            public object Invoke(object[] arguments)
+            {
+                return null;
+            }
+        }
 
         #endregion
     }
