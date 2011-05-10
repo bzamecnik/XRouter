3ddd7300d6490ab54deffe3f7baa0b56e8b24e9b
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2011-05-10 13:36:24 +0200

    Create some tests for DaemonNT.
    Unit tests of ConfigProvider, system tests of installing/running services.

---
+++ DaemonNT/DaemonNT.Test/ConfigProviderTest.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DaemonNT.Configuration;
+using Xunit;
+
+namespace DaemonNT.Test
+{
+    public class ConfigProviderTest
+    {
+        // TODO:
+        // - good day:
+        //   - default config file
+        // - bad day:
+        //   - make bad schemas according to asserts in config_schema.xml
+        //     (about 65 asserts :) )
+        //   - non-existent config file
+
+        #region Happy-day tests
+
+        [Fact]
+        public void LoadGoodFullConfigSingleService()
+        {
+            ServiceSettings settings = ConfigProvider.LoadServiceSettings(
+                "FooService",
+                @"ConfigProviderFiles\GoodFullConfigSingleService.xml");
+
+            Assert.NotNull(settings);
+            Assert.Equal("DaemonNT.FooService", settings.TypeClass);
+            Assert.Equal(@"..\..\..\DaemonNT.Test\bin\Debug\DaemonNT.FooService.dll",
+                settings.TypeAssembly);
+
+            #region Service settings
+
+            var serviceSettings = settings.Settings;
+
+            Assert.NotNull(serviceSettings);
+            Assert.Equal("Service.1", serviceSettings.Parameter["SettingsParam"]);
+            Assert.NotNull(serviceSettings["Section1"]);
+            Assert.Equal("Service.2", serviceSettings["Section1"]
+                .Parameter["Section1Param"]);
+            Assert.NotNull(serviceSettings["Section1"]["InnerSection"]);
+            Assert.Equal("Service.3", serviceSettings["Section1"]
+                ["InnerSection"].Parameter["InnerSectionParam"]);
+
+            #endregion
+
+            #region Installer settings
+
+            Assert.NotNull(settings.InstallerSettings);
+            var installerSettings = settings.InstallerSettings;
+            Assert.Equal("Service description", installerSettings.Description);
+            Assert.Equal("User", installerSettings.Account);
+            Assert.Equal("User name", installerSettings.User);
+            Assert.Equal("Password", installerSettings.Password);
+            Assert.Equal("Automatic", installerSettings.StartMode);
+
+            Assert.NotNull(installerSettings.RequiredServices);
+            var requiredServices = installerSettings.RequiredServices;
+            Assert.Equal(3, requiredServices.Count());
+            Assert.Contains("Service1", requiredServices);
+            Assert.Contains("Service2", requiredServices);
+            Assert.Contains("Service3", requiredServices);
+
+            #endregion
+
+            #region Trace-logger settings
+
+            var loggerSettings = settings.TraceLoggerSettings;
+
+            Assert.NotNull(loggerSettings);
+            Assert.NotNull(loggerSettings.Storages);
+            Assert.Equal(1, loggerSettings.Storages.Count);
+            Assert.NotNull(loggerSettings.Storages[0]);
+
+            var storageSettings = settings.TraceLoggerSettings.Storages[0].Settings;
+
+            Assert.NotNull(storageSettings);
+            Assert.Equal("TraceLoggerStorage.1", storageSettings.Parameter["SettingsParam"]);
+            Assert.NotNull(storageSettings["Section1"]);
+            Assert.Equal("TraceLoggerStorage.2", storageSettings["Section1"]
+                .Parameter["Section1Param"]);
+            Assert.NotNull(storageSettings["Section1"]["InnerSection"]);
+            Assert.Equal("TraceLoggerStorage.3", storageSettings["Section1"]
+                ["InnerSection"].Parameter["InnerSectionParam"]);
+
+            #endregion
+        }
+
+        #endregion
+
+        #region Bad-day tests
+
+        [Fact]
+        public void LoadConfigOfNonexistentService()
+        {
+            // should throw InvalidOperationException:
+            // "The supplied service.name='BarService' is not specified in
+            // ConfigProviderFiles\ConfigWithASingleEmptyService.xml."
+            Assert.Throws<InvalidOperationException>(() =>
+                ConfigProvider.LoadServiceSettings("BarService",
+                    @"ConfigProviderFiles\ConfigWithASingleEmptyService.xml"));
+        }
+
+        [Fact]
+        public void LoadNonexistentConfigFile()
+        {
+            Assert.Throws<System.IO.FileNotFoundException>(() =>
+                ConfigProvider.LoadServiceSettings("FooService",
+                    @"ConfigProviderFiles\NonexistentConfigFile.xml"));
+        }
+
+        #endregion
+    }
+}
---
+++ DaemonNT/DaemonNT.Test/ServiceHostingTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using DaemonNT;
+using Xunit;
+
+namespace DaemonNT.Test
+{
+    class ServiceHostingTest
+    {
+        // TODO:
+        // - test the following:
+        //   - installing an NT service
+        //     - if it is not installed -> good
+        //     - if it is already installed -> bad
+        //   - uninstalling an NT service
+        //     - if it is already installed -> good
+        //     - if it is not installed -> bad
+        //   - running an NT service
+        //     - if it is already installed -> good
+        //     - if it is not installed -> bad
+        //   - running a service in debug mode
+        //
+        // Check the NT service status with:
+        //    using System.ServiceProcess;
+        //    ServiceController sc = new ServiceController(SERVICENAME);
+        //    switch (sc.Status) { ... }
+
+        [Fact]
+        public void InstallNotAlreadyInstalledService()
+        {
+            string serviceName = "MyServer";
+            ServiceController sc = new ServiceController(serviceName);
+
+            new ServiceCommands().Install(serviceName);
+
+            try
+            {
+                ServiceControllerStatus status = sc.Status;
+                Assert.Equal(ServiceControllerStatus.Stopped, status);
+            }
+            catch (Exception ex)
+            {
+                Assert.True(false);
+            }
+        }
+
+        [Fact]
+        public void RunInstalledService()
+        {
+            string serviceName = "MyServer";
+            ServiceController sc = new ServiceController(serviceName);
+
+            Console.WriteLine(sc.Status);
+
+            new ServiceCommands().Run(serviceName);
+
+
+            Console.WriteLine(sc.Status);
+        }
+
+        [Fact]
+        public void GetServiceStatus()
+        {
+            ServiceController sc = new ServiceController("MyServer");
+            Console.WriteLine("MyServer: {0}", sc.Status);
+        }
+    }
+}
