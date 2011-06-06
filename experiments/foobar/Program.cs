fbbb86344e4324b2671697a820e5af2eeb39f7b7
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2011-06-06 11:49:05 +0200

    Git workshop: Bar() method.

--- experiments/foobar/Program.cs
+++ experiments/foobar/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +10,8 @@ namespace foobar
         static void Main(string[] args)
         {
         }
+        
+        public void Bar(int barParam) {
+				}
     }
 }
