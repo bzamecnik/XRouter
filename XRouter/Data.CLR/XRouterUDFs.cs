155e42446c5ef6cc662b3fa4fc1b58f350702d58
Petr Sobotka <petersob@seznam.cz>
2011-07-04 23:49:20 +0200

    CLR functions for data layer added

---
+++ XRouter/Data.CLR/XRouterUDFs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Collections;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+
+namespace XRouter.Data.CLR
+{
+    public partial class XRouterUDFs
+    {
+        [SqlFunction(
+            Name = "Split",
+            FillRowMethodName = "FillRow",
+            TableDefinition = "value nvarchar(MAX)"
+        )]
+        public static IEnumerable Split(SqlString input, SqlString delimeter, SqlBoolean removeEmpty)
+        {
+            if (input.IsNull || delimeter.IsNull || removeEmpty.IsNull)
+            {
+                return null;
+            }
+
+            StringSplitOptions stringSplitOptions;
+            if (removeEmpty)
+            {
+                stringSplitOptions = StringSplitOptions.RemoveEmptyEntries;
+            }
+            else
+            {
+                stringSplitOptions = StringSplitOptions.None;
+            }
+
+            return input.Value.Split(delimeter.Value.ToCharArray(), stringSplitOptions);
+        }
+
+        public static void FillRow(object row, out SqlString input)
+        {
+            input = new SqlString((string)row);
+        }
+    };
+}
+
