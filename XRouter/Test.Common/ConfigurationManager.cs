94af29092c891d34254dabce967da9a16493f89c
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2011-07-27 13:05:51 +0200

    XRouter tests: support loading message flows and XRM items from files.
    Add some example files.

--- XRouter/Test.Common/ConfigurationManager.cs
+++ XRouter/Test.Common/ConfigurationManager.cs
@@ -1,4 +1,9 @@
-﻿using System.Xml;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
 using XRouter.Common.ComponentInterfaces;
 using wcf = System.ServiceModel;
 
@@ -14,6 +19,73 @@ namespace XRouter.Test
             binding.ReaderQuotas = new XmlDictionaryReaderQuotas() { MaxBytesPerRead = int.MaxValue, MaxArrayLength = int.MaxValue, MaxStringContentLength = int.MaxValue };
             wcf.ChannelFactory<IBrokerServiceForManagement> channelFactory = new wcf.ChannelFactory<IBrokerServiceForManagement>(binding, endpointAddress);
             return channelFactory.CreateChannel();
+        }
+
+        /// <summary>
+        /// Creates a XML resource storage and loads all requested items
+        /// from files into the storage.
+        /// </summary>
+        /// <param name="resourceNames"></param>
+        /// <param name="testName"></param>
+        /// <returns></returns>
+        public static XElement LoadXrmItems(IEnumerable<string> resourceNames, string testName)
+        {
+            XElement storage = new XElement("xml-resource-storage");
+            foreach (string resource in resourceNames)
+            {
+                XElement item = LoadXrmItemFromFile(resource, testName);
+                storage.Add(item);
+            }
+            return storage;
+        }
+
+        /// <summary>
+        /// Loads a XML file. First try test-specific location, if it
+        /// is not available try a common location.
+        /// </summary>
+        /// <param name="resourceName"></param>
+        /// <param name="subPath"></param>
+        /// <param name="testName"></param>
+        /// <returns></returns>
+        private static XDocument LoadXmlFile(string resourceName, string subPath, string testName)
+        {
+            if (!Regex.IsMatch(resourceName, @"[a-zA-Z0-9_\-]+"))
+            {
+                throw new ArgumentException();
+            }
+            string fileName = string.Format(@"Data\{0}\{1}\{2}.xml", testName, subPath, resourceName);
+            if (!File.Exists(fileName))
+            {
+                fileName = string.Format(@"Data\Common\{0}\{1}.xml", subPath, resourceName);
+            }
+            return XDocument.Load(fileName);
+        }
+
+        /// <summary>
+        /// Loads a XML resource item from a file.
+        /// </summary>
+        /// <param name="resourceName"></param>
+        /// <param name="testName"></param>
+        /// <returns></returns>
+        private static XElement LoadXrmItemFromFile(string resourceName, string testName)
+        {
+            XDocument doc = LoadXmlFile(resourceName, @"config\XRM", testName);
+            XElement item = new XElement("item");
+            item.SetAttributeValue("name", resourceName);
+            item.Add(doc.Root);
+            return item;
+        }
+
+        /// <summary>
+        /// Loads a message flow configuration from a file.
+        /// </summary>
+        /// <param name="resourceName"></param>
+        /// <param name="testName"></param>
+        /// <returns></returns>
+        public static XElement LoadMessageFlowFromFile(string resourceName, string testName)
+        {
+            XDocument doc = LoadXmlFile(resourceName, @"config\MessageFlow", testName);
+            return doc.Root;
         }
     }
 }
--- XRouter/Test.Integration/BasicXRouterPipelineTests.cs
+++ XRouter/Test.Integration/BasicXRouterPipelineTests.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Xml.Linq;
 using System.Xml.XPath;
 using XRouter.Common.ComponentInterfaces;
 using XRouter.Common.MessageFlowConfig;
+using XRouter.Common.Utils;
 using XRouter.Processor.MessageFlowBuilding;
 using Xunit;
 
 namespace XRouter.Test.Integration
 {
+    // TODO: load message flow configs from files
+
     public class BasicXRouterPipelineTests
     {
         /// <summary>
@@ -28,47 +34,53 @@ namespace XRouter.Test.Integration
         [Fact]
         public void CreateSimpleMessageFlow()
         {
-            #region Create message flow
-            var terminator = MessageFlowBuilder.CreateTerminator("termination");
-            var send = MessageFlowBuilder.CreateSender("sendToA",
-                inputMessage: "input", outputEndpoint: "OutA", nextNode: terminator);
-            var messageFlowConfig = new MessageFlowConfiguration("sendToA", 1)
-            {
-                Nodes = { send, terminator },
-                RootNode = send
-            };
-            #endregion
-            
+            //#region Create message flow
+            //var terminator = MessageFlowBuilder.CreateTerminator("termination");
+            //var send = MessageFlowBuilder.CreateSender("sendToA",
+            //    inputMessage: "input", outputEndpoint: "OutA", nextNode: terminator);
+            //var messageFlowConfig = new MessageFlowConfiguration("sendToA", 1)
+            //{
+            //    Nodes = { send, terminator },
+            //    RootNode = send
+            //};
+            //#endregion
+
+            XElement xMessageFlow = ConfigurationManager.LoadMessageFlowFromFile("simple-flow", "Test1");
+            MessageFlowConfiguration messageFlowConfig = XSerializer.Deserialize<MessageFlowConfiguration>(xMessageFlow);
+
             IBrokerServiceForManagement broker = ConfigurationManager.GetBrokerServiceProxy();
             // load current configuration
             var configuration = broker.GetConfiguration();
             var xConfig = configuration.Content.XDocument;
 
             // remove all message flows
-            xConfig.XPathSelectElements("/configuration/messageflows/messageflow").Remove();
-            
+            xConfig.XPathSelectElement("/configuration/messageflows").RemoveNodes();
+
             // add current message flow
             configuration.AddMessageFlow(messageFlowConfig);
             configuration.SetCurrentMessageFlowGuid(messageFlowConfig.Guid);
 
             // remove all previous XRM items
-            xConfig.XPathSelectElements("/configuration/xml-resource-storage/item").Remove();
+            xConfig.XPathSelectElement("/configuration/xml-resource-storage").RemoveNodes();
 
             // add needed XRM items
-            configuration.SaveXrmContent(XElement.Parse(@"
-<xml-resource-storage>
-    <item name='schematron1'>
-        <xsl:stylesheet version='1.0' xmlns:xsl='http://www.w3.org/1999/XSL/Transform' xmlns:S='http://www.w3.org/2003/05/soap-envelope'>
-            <xsl:template match='node()|@*'>
-                <xsl:copy>
-                    <xsl:apply-templates select='node()|@*' />
-                </xsl:copy>
-            </xsl:template>
-            <xsl:template match='S:Header' />
-        </xsl:stylesheet>
-    </item>
-</xml-resource-storage>
-          "));
+//            var xrm = XElement.Parse(@"
+//<xml-resource-storage>
+//    <item name='schematron1'>
+//        <xsl:stylesheet version='1.0' xmlns:xsl='http://www.w3.org/1999/XSL/Transform' xmlns:S='http://www.w3.org/2003/05/soap-envelope'>
+//            <xsl:template match='node()|@*'>
+//                <xsl:copy>
+//                    <xsl:apply-templates select='node()|@*' />
+//                </xsl:copy>
+//            </xsl:template>
+//            <xsl:template match='S:Header' />
+//        </xsl:stylesheet>
+//    </item>
+//</xml-resource-storage>
+//          ");
+            var xrm = ConfigurationManager.LoadXrmItems(
+                new[] { "sample1a", "sample1b", "sample1c" }, "Test1");
+            configuration.SaveXrmContent(xrm);
 
             // update the configuration
 
