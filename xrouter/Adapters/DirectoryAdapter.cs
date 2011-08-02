0efb1c9070f9706bfa848077058cd823e9c0a0bf
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2011-08-02 16:38:57 +0200

    DirectoryAdapter: restore behavior or ignoring problematic input files.
    When there's a problem reading input files or directories, they should be ignored, not logged in a loop.
    
    Restore changes from commit df3f7596cd811f1117a796b23126e17f680c8dee lost in commit d306b93c73b8a8c5bff1ed0913efef2035cc65c9.

--- xrouter/Adapters/DirectoryAdapter.cs
+++ xrouter/Adapters/DirectoryAdapter.cs
@@ -45,10 +45,12 @@ namespace XRouter.Adapters
             System.Diagnostics.Debug.Assert(outputEndpointToPathMap != null);
 
             #region Watch input directories
-            while (IsRunning) {
+            while (IsRunning)
+            {
                 Thread.Sleep(TimeSpan.FromSeconds(checkingIntervalInSeconds));
 
-                foreach (var enpointNameAndPath in inputEndpointToPathMap) {
+                foreach (var enpointNameAndPath in inputEndpointToPathMap)
+                {
                     string enpointName = enpointNameAndPath.Key;
                     string path = enpointNameAndPath.Value;
 
@@ -59,54 +61,51 @@ namespace XRouter.Adapters
                     }
                     catch (Exception ex)
                     {
-                        EventLog.Error(string.Format(
-                            "Cannot read files from directory: {0}. Details: {1}",
-                            path, ex.Message));
+                        // Ignore input directories which cannot be read.
                         continue;
                     }
-                    foreach (string newFilePath in newFiles) {
+                    foreach (string newFilePath in newFiles)
+                    {
                         try
                         {
-                        string fileContent;
-                        try
-                        {
-                            fileContent = File.ReadAllText(newFilePath);
-                        }
-                        catch (Exception)
-                        {
-                            continue;
-                        }
-                        XDocument message = XDocument.Parse(fileContent);
+                            string fileContent;
+                            try
+                            {
+                                fileContent = File.ReadAllText(newFilePath);
+                            }
+                            catch (Exception)
+                            {
+                                continue;
+                            }
+                            XDocument message = XDocument.Parse(fileContent);
 
-                        XDocument metadata = new XDocument();
-                        XElement xMetadata = new XElement(XName.Get("file-metadata"));
-                        xMetadata.SetAttributeValue("filename", Path.GetFileName(newFilePath));
-                        metadata.Add(xMetadata);
+                            XDocument metadata = new XDocument();
+                            XElement xMetadata = new XElement(XName.Get("file-metadata"));
+                            xMetadata.SetAttributeValue("filename", Path.GetFileName(newFilePath));
+                            metadata.Add(xMetadata);
 
-                        TraceLog.Info("Found input file " + Path.GetFileName(newFilePath));
-                        try
-                        {
-                            ReceiveMessageXml(message, enpointName, metadata);
+                            TraceLog.Info("Found input file " + Path.GetFileName(newFilePath));
+                            try
+                            {
+                                ReceiveMessageXml(message, enpointName, metadata);
+                            }
+                            catch (Exception ex)
+                            {
+                                // message receiving failed
+                                continue;
+                            }
+
+                            // tady by bylo dobre, aby nedochazelo ke ztrate nebo rozmnozovani zprav
+                            // navrhuji udelat to, ze jakmile bude zprava ulozena v databazi, tak adpter dostane notifikaci
+                            // a zavola se nasledujici prikaz (takova notifikace se muze hodit i u synchronnich adapteru, 
+                            // ale u WebServices to neni nutne
+
+                            File.Delete(newFilePath);
                         }
                         catch (Exception ex)
                         {
-                            // message receiving failed
+                            // Ignore input files which cannot be read.
                             continue;
-                        }
-                     
-                        // tady by bylo dobre, aby nedochazelo ke ztrate nebo rozmnozovani zprav
-                        // navrhuji udelat to, ze jakmile bude zprava ulozena v databazi, tak adpter dostane notifikaci
-                        // a zavola se nasledujici prikaz (takova notifikace se muze hodit i u synchronnich adapteru, 
-                        // ale u WebServices to neni nutne
-
-                        File.Delete(newFilePath);
-                        }
-                        catch (Exception ex)
-                        {
-                            EventLog.Error(string.Format(
-                                "Error while receiving file {0}. Details: {1}",
-                                newFilePath, ex.Message));
-                            // TODO: protentially store bad file contents into TraceLog
                         }
                     }
                 }
@@ -117,19 +116,23 @@ namespace XRouter.Adapters
         public override XDocument SendMessage(string endpointName, XDocument message, XDocument metadata)
         {
             string targetPath;
-            if (outputEndpointToPathMap.TryGetValue(endpointName, out targetPath)) {
+            if (outputEndpointToPathMap.TryGetValue(endpointName, out targetPath))
+            {
                 string fileName;
 
-                if (metadata != null) {
+                if (metadata != null)
+                {
                     // tohle by bylo dobre, ale chtelo by to, aby tam uzivatel mohl specifikovat substituce (poresime osobne)                    
                     fileName = metadata.Element(XName.Get("file-metadata")).Attribute(XName.Get("filename")).Value;
-                } else {
+                }
+                else
+                {
                     // tady poresime ziskani defaultniho jednoznacneho 
-                    fileName = string.Format(DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss.xml"));                    
+                    fileName = string.Format(DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss.xml"));
                 }
 
                 TraceLog.Info(string.Format("Writing output file '{0}' into '{1}'", fileName, targetPath));
-                                
+
                 // tady musime zase osetrit to, aby se neprepisovaly zpravy, udelame to generovanim jednoznacnych nazvu souboru
                 File.WriteAllText(Path.Combine(targetPath, fileName), message.ToString());
             }
