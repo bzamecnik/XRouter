c05843d3f2f40d4bf47359d06e00712157b7f1cc
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2011-08-06 01:41:30 +0200

    XRouter: add some doc comments.

--- xrouter/Common/Xrm/IXmlStorage.cs
+++ xrouter/Common/Xrm/IXmlStorage.cs
@@ -3,11 +3,28 @@ using System.Xml.Linq;
 
 namespace XRouter.Common.Xrm
 {
+    /// <summary>
+    /// Represents a XML storage with a single document which is able to
+    /// notify when the contents has changed.
+    /// </summary>
     public interface IXmlStorage
     {
+        /// <summary>
+        /// Fired when the stored XML document has changed.
+        /// </summary>
         event Action XmlChanged;
 
+        /// <summary>
+        /// Stores the provided XML document into the storage replacing the
+        /// possible existing one.
+        /// </summary>
+        /// <param name="xml">new XML document</param>
         void SaveXml(XDocument xml);
+
+        /// <summary>
+        /// Loads the XML document from the storage
+        /// </summary>
+        /// <returns>XML document stored in the storage; can be null</returns>
         XDocument LoadXml();
     }
 }
