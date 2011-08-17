a17b72eae2b1ed2d2ec554e8b01677ff30a16f71
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2011-08-17 11:38:41 +0200

    XRouter demo: add a skeleton of the Library demo.

---
+++ XRouterExamples/Library/Library/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Linq;
+using System.Text;
+
+namespace Library
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+        public string Authors { get; set; }
+        public string Desciption { get; set; }
+        public string YearPublished { get; set; }
+        public string Publisher { get; set; }
+        public bool Available { get; set; }
+
+        public static Book FromXml(XElement xml)
+        {
+            Book book = new Book();
+            book.Id = Guid.Parse(xml.Attribute("id").Value);
+            book.Isbn = xml.XPathSelectElement("/book/isbn").Value;
+            book.Title = xml.XPathSelectElement("/book/title").Value;
+            book.Authors = xml.XPathSelectElement("/book/authors").Value;
+            book.Desciption = xml.XPathSelectElement("/book/desciption").Value;
+            book.YearPublished = xml.XPathSelectElement("/book/yearPublished").Value;
+            book.Publisher = xml.XPathSelectElement("/book/publisher").Value;
+            book.Available = Boolean.Parse(xml.XPathSelectElement("/book/available").Value);
+            return book;
+        }
+
+        public XElement ToXml()
+        {
+            XElement xBook = new XElement("reader");
+            xBook.SetAttributeValue("id", Id);
+            xBook.Add(new XElement("isbn") { Value = Isbn });
+            xBook.Add(new XElement("title") { Value = Title });
+            xBook.Add(new XElement("authors") { Value = Authors });
+            xBook.Add(new XElement("desciption") { Value = Desciption });
+            xBook.Add(new XElement("yearPublished") { Value = YearPublished });
+            xBook.Add(new XElement("publisher") { Value = Publisher });
+            xBook.Add(new XElement("available") { Value = Available.ToString() });
+            return xBook;
+        }
+    }
+}
---
+++ XRouterExamples/Library/Library/Loan.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Linq;
+using System.Text;
+
+namespace Library
+{
+    public class Loan
+    {
+        public Guid Id { get; set; }
+        public Guid ReaderId { get; set; }
+        public Guid BookId { get; set; }
+        public string Status { get; set; }
+        public DateTime DateLoaned { get; set; }
+        public DateTime DateLoanDeadline { get; set; }
+        public DateTime DateReturned { get; set; }
+
+        public static Loan FromXml(XElement xml)
+        {
+            Loan loan = new Loan();
+            loan.Id = Guid.Parse(xml.Attribute("id").Value);
+            loan.ReaderId = Guid.Parse(xml.Attribute("readerId").Value);
+            loan.BookId = Guid.Parse(xml.Attribute("bookId").Value);
+            loan.Status = xml.XPathSelectElement("/loan/status").Value;
+            loan.DateLoaned = DateTime.Parse(xml.XPathSelectElement("/loan/dateLoaned").Value);
+            loan.DateLoanDeadline = DateTime.Parse(xml.XPathSelectElement("/loan/dateLoanDeadline").Value);
+            loan.DateReturned = DateTime.Parse(xml.XPathSelectElement("/loan/dateReturned").Value);
+            return loan;
+        }
+
+        public XElement ToXml()
+        {
+            XElement xLoan = new XElement("loan");
+            xLoan.SetAttributeValue("id", Id);
+            xLoan.SetAttributeValue("readerId", ReaderId);
+            xLoan.SetAttributeValue("bookId", BookId);
+            xLoan.Add(new XElement("status") { Value = Status });
+            xLoan.Add(new XElement("dateLoaned") { Value = DateLoaned.ToString() });
+            xLoan.Add(new XElement("dateLoanDeadline") { Value = DateLoanDeadline.ToString() });
+            xLoan.Add(new XElement("dateReturned") { Value = DateReturned.ToString() });
+            return xLoan;
+        }
+    }
+}
