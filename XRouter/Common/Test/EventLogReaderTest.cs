ec281f5228c8c9c3f5198330cbf55eeb9c030846
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2011-08-18 14:11:47 +0200

    XRouter: create some manual log reader tests.

--- XRouter/Common/Test/EventLogReaderTest.cs
+++ XRouter/Common/Test/EventLogReaderTest.cs
@@ -2,10 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Xunit;
 
 namespace XRouter.Common.Test
 {
     class EventLogReaderTest
     {
+        public void ReadLogs()
+        {
+            EventLogReader reader = new EventLogReader(
+                "TestLogs", "xrouter");
+            var entries = reader.GetEntries(
+                new DateTime(2011, 1, 1),
+                new DateTime(2012, 1, 1),
+                LogLevelFilters.Error | LogLevelFilters.Warning | LogLevelFilters.Info,
+                int.MaxValue, 1);
+            int total = entries.Count();
+            Console.WriteLine("Total: " + total);
+
+            entries = reader.GetEntries(
+                new DateTime(2011, 1, 1),
+                new DateTime(2012, 1, 1),
+                LogLevelFilters.Error,
+                int.MaxValue, 1);
+            int error = entries.Count();
+            Console.WriteLine("Error: " + total);
+            entries = reader.GetEntries(
+                new DateTime(2011, 1, 1),
+                new DateTime(2012, 1, 1),
+                LogLevelFilters.Warning,
+                int.MaxValue, 1);
+            int warning = entries.Count();
+            Console.WriteLine("Warning: " + total);
+            entries = reader.GetEntries(
+                new DateTime(2011, 1, 1),
+                new DateTime(2012, 1, 1),
+                LogLevelFilters.Info,
+                int.MaxValue, 1);
+            int info = entries.Count();
+            Console.WriteLine("Info: " + total);
+            Assert.Equal(total, error + warning + info);
+        }
     }
 }
---
+++ XRouter/Common/Test/TraceLogReaderTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace XRouter.Common.Test
+{
+    class TraceLogReaderTest
+    {
+        public void ReadLogs()
+        {
+            TraceLogReader reader = new TraceLogReader(
+                "TestLogs", "xroutermanager");
+            var entries = reader.GetEntries(
+                new DateTime(2011, 1, 1),
+                new DateTime(2012, 1, 1),
+                LogLevelFilters.Error | LogLevelFilters.Warning | LogLevelFilters.Info,
+                int.MaxValue, 1);
+            int total = entries.Count();
+            Console.WriteLine("Total: " + total);
+        }
+    }
+}
