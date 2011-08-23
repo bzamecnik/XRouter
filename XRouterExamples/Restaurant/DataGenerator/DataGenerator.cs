2554ea19e543e67fde7690bdea3ad1c1cc80780a
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2011-08-23 18:52:25 +0200

    XRouter demo: refactor Restaurant.DataGenerator.

---
+++ XRouterExamples/Restaurant/DataGenerator/DataGenerator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Xml.Linq;
+using System.Text;
+
+namespace XRouter.Examples.Restaurant.DataGenerator
+{
+    class DataGenerator
+    {
+        public int MaxFoodId { get; set; }
+        public int MaxDrinkId { get; set; }
+
+        public string OutputPath { get; set; }
+
+        Random random = new Random();
+
+        public void GeneratePayments(int tableCount)
+        {
+            for (int table = 1; table <= tableCount; ++table)
+            {
+                XElement payment = new XElement("payment", new XElement("table", table));
+                XDocument doc = new XDocument();
+                doc.Add(payment);
+
+                string fileName = string.Format("paymentForTable{0}.xml", table.ToString().PadLeft(2, '0'));
+                doc.Save(Path.Combine(OutputPath, fileName));
+            }
+        }
+
+        public void GenerateOrders(int orderCount, int tableCount)
+        {
+            for (int orderNumber = 1; orderNumber <= orderCount; ++orderNumber)
+            {
+                XElement order = new XElement("order");
+                int tableId = random.Next(1, tableCount);
+                order.Add(new XElement("table", tableId));
+
+                string ItemId;
+                int maxId;
+                if (random.Next(0, 1) == 0)
+                {
+                    ItemId = "food";
+                    maxId = MaxFoodId;
+                }
+                else
+                {
+                    ItemId = "drink";
+                    maxId = MaxDrinkId;
+                }
+                ItemId += random.Next(1, maxId).ToString().PadLeft(4, '0');
+                order.Add(new XElement("item", ItemId));
+
+                XDocument doc = new XDocument();
+                doc.Add(order);
+
+                string fileName = string.Format("order{0}.xml", orderNumber.ToString().PadLeft(5, '0'));
+                doc.Save(Path.Combine(OutputPath, fileName));
+            }
+        }
+    }
+}
--- XRouterExamples/Restaurant/DataGenerator/Program.cs
+++ XRouterExamples/Restaurant/DataGenerator/Program.cs
@@ -8,6 +8,8 @@ using System.Xml.Linq;
 
 namespace XRouter.Examples.Restaurant.DataGenerator
 {
+    // identifier are counted from 1
+
     class Program
     {
         static void Main(string[] args)
@@ -15,53 +17,39 @@ namespace XRouter.Examples.Restaurant.DataGenerator
             int tables = 10;
             int orders = 50;
 
-            int maxFoodID = 9;
-            int maxDrinkID = 5;
+            int maxFoodId = 9;
+            int maxDrinkId = 5;
 
-            string savePath = @"Data\Generated\";
+            string savePath = @"Data\Generated";
+
+            if (args.Length == 5)
+            {
+                savePath = args[0];
+                Int32.TryParse(args[1], out tables);
+                Int32.TryParse(args[2], out orders);
+                Int32.TryParse(args[3], out maxFoodId);
+                Int32.TryParse(args[4], out maxDrinkId);
+            }
 
             if (!Directory.Exists(savePath))
             {
                 Directory.CreateDirectory(savePath);
             }
 
-            Random random = new Random();
-
-            Console.WriteLine("Generating orders...");
-            for (int orderNumber = 1; orderNumber <= orders; ++orderNumber)
+            DataGenerator gen = new DataGenerator()
             {
-                XDocument doc = new XDocument();
-
-                XElement order = new XElement("order");
-                order.Add(new XElement("table", random.Next(1, tables)));
-
-                string ItemId;
-                if (random.Next(0, 1) == 0)
-                {
-                    ItemId = "food" + random.Next(1, maxFoodID).ToString().PadLeft(4, '0');
-                }
-                else
-                {
-                    ItemId = "drink" + random.Next(1, maxDrinkID).ToString().PadLeft(4, '0');
-                }
-                order.Add(new XElement("item", ItemId));
-
-                doc.Add(order);
-                doc.Save(savePath + "order" + orderNumber.ToString().PadLeft(5, '0') + ".xml");
-            }
-            Console.WriteLine("Orders have been generated");
+                MaxDrinkId = maxDrinkId,
+                MaxFoodId = maxFoodId,
+                OutputPath = savePath
+            };
 
             Console.WriteLine("Generating payments...");
-            for (int table = 1; table <= tables; ++table)
-            {
-                XDocument doc = new XDocument();
-
-                XElement payment = new XElement("payment", new XElement("table", table));
-
-                doc.Add(payment);
-                doc.Save(savePath + "paymentForTable" + table.ToString().PadLeft(2, '0') + ".xml");
-            }
+            gen.GeneratePayments(tables);
             Console.WriteLine("Payments have been generated");
+
+            Console.WriteLine("Generating orders...");
+            gen.GenerateOrders(orders, tables);
+            Console.WriteLine("Orders have been generated");
 
             Console.WriteLine("Press a key...");
             Console.ReadKey();
