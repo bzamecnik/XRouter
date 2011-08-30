463d322989fa9c592d8fc892830d8fd4c30e7c83
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2011-08-31 00:12:01 +0200

    Docs: create covers for each volume, add XRouter logo.

--- docs/cover/make-cover.bat
+++
@@ -1,2 +0,0 @@
-rem LaTeX is required (eg. the latest TexLive)
-pdfcslatex cover.tex
---
+++ docs/cover/make-covers.bat
@@ -0,0 +1,8 @@
+rem LaTeX is required (eg. the latest TexLive)
+pdfcslatex cover_main.tex
+pdfcslatex cover_01_definition.tex
+pdfcslatex cover_02_programmers_guide.tex
+pdfcslatex cover_03_users_guide.tex
+pdfcslatex cover_04_administrators_guide.tex
+pdfcslatex cover_05_schematron.tex
+pdfcslatex cover_06_daemonnt.tex
